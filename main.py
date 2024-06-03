920d2f8944111aac5e2a504045a1bd683258fb29
andrehsilva <andrehrsilva@gmail.com>
2024-06-03 10:50:39 -0300

    inicio

---
+++ main.py
@@ -0,0 +1,117 @@
+import streamlit as st
+import io
+import pandas as pd
+
+
+st.set_page_config(
+        page_title="Annual Meeting",
+        page_icon="large_blue_square",
+        layout="wide",
+    )
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+
+##########################################################################################################################################################
+##########################################################################################################################################################
+
+if check_password():
+    buffer = io.BytesIO()
+
+    st.header('Annual 2024')
+    st.subheader('Mapa da escola e possível Upsell:')
+    df = pd.read_excel('dados.xlsx')
+
+    # Cria uma lista de escolas para o selectbox
+    escolas = df['School Name'].unique()
+    
+    # Input para digitar parte do nome da escola
+    escola_parte = st.text_input("Pesquise por parte do nome da escola")
+    # Filtra a lista de escolas baseado no input
+    escolas_filtradas = [escola for escola in escolas if escola_parte.lower() in escola.lower()]
+    
+    # Adiciona uma opção vazia no início da lista de escolas filtradas
+    escolas_filtradas.insert(0, "")
+
+    # Selectbox para escolher a escola das opções filtradas
+    escola_selecionada = st.selectbox("Escolha uma escola", escolas_filtradas)
+    if escola_selecionada:
+        # Filtra o DataFrame pela escola selecionada
+        df_filtrado = df[df['School Name'] == escola_selecionada]
+        
+        # Exibir quantidade de alunos (supondo que a coluna seja 'Número de Alunos')
+        ei = df_filtrado['Alunos Educação Infantil'].sum() if 'Alunos Educação Infantil' in df_filtrado.columns else 'Informação não disponível'
+        ef1 = df_filtrado['Alunos Educação Anos Iniciais'].sum() if 'Alunos Educação Anos Iniciais' in df_filtrado.columns else 'Informação não disponível'
+        ef2 = df_filtrado['Alunos Educação Anos Finais'].sum() if 'Alunos Educação Anos Finais' in df_filtrado.columns else 'Informação não disponível'
+        em = df_filtrado['Alunos Ensino Médio'].sum() if 'Alunos Ensino Médio' in df_filtrado.columns else 'Informação não disponível'
+        pv = df_filtrado['Alunos Pré Vestibular'].sum() if 'Alunos Pré Vestibular' in df_filtrado.columns else 'Informação não disponível'
+
+        # Criação de colunas para exibir os valores
+        col1, col2, col3, col4, col5 = st.columns(5)
+        
+        col1.metric(label="EI", value=ei)
+        col2.metric(label="EFI", value=ef1)
+        col3.metric(label="EFII", value=ef2)
+        col4.metric(label="EM", value=em)
+        col5.metric(label="PV", value=pv)
+
+        st.write(f"{escola_selecionada}")
+
+        # Exibe todos os produtos da escola selecionada
+        st.write(f"Marcas contratadas:")
+        
+        sistemas = df_filtrado.iloc[:, 3:-17]  # Seleciona todas as colunas de produtos a partir da quinta coluna
+        st.dataframe(sistemas, hide_index=True)
+
+        # Exibe todos os produtos da escola selecionada
+        st.write(f"Produtos da escola:")
+
+        produtos = df_filtrado.iloc[:, 6:-9]  # Seleciona todas as colunas de produtos a partir da quinta coluna
+        st.dataframe(produtos, hide_index=True)
+
+        # Exibe todas as novas soluções educacionais
+        st.write(f"Novas soluções educacionais:")
+
+        col1, col2, col3, col4 = st.columns(4)
+        
+        col1.write('- Aprova rápido')
+        col2.write('- Alura Start')
+        col3.write('- Adapte')
+        col4.write('- Sponte')
+    else:
+        st.write("Nenhuma escola encontrada com esse nome.")
+
+
+
+
+    
+    
+     
+             
